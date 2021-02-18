6ea0f37324e2f5a09a728ead1b279251ca8918aa
Erik Brown <ejbrownlf@gmail.com>
2021-02-18 14:48:00 -0600

    updated test with new format for the office number

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,8 +5,9 @@ describe('getRole', () => {
     it('updates role to manager and sets the office number to the inputed value', () => {
         const employee = new Employee();
         const manager = new Manager();
-        manager.getRole(employee, 123);
-        expect(employee).toEqual({"name": "", "id": "", "email": "", "role": "Manager", "officeNumber": 123});
+        const number = 123
+        manager.getRole(employee, number);
+        expect(employee).toEqual({"name": "", "id": "", "email": "", "role": "Manager", "officeNumber": `Office Number: ${number}`});
     });
     
 });
